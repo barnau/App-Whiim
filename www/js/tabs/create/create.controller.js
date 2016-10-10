b96914e0b5604fd55c88ea3513d9e81ce2d56570
Michele Kempinsky <mkempinsky@icloud.com>
2016-10-10 10:54:09 -0700

    working posting event

--- www/js/tabs/create/create.controller.js
+++ www/js/tabs/create/create.controller.js
@@ -11,44 +11,63 @@
     function CreateController(FirebaseFactory, $stateParams, $state, $scope, $ionicPopover, $sessionStorage, toastr) {
         var vm = this;
         vm.title = 'CreateController';
-        vm.event = {};
+        vm.newEvent = {};
         vm.eventName = '';
         vm.requiredUsers = 0;
         vm.description = ''; 
-        vm.category = '';
         vm.createEvent = createEvent;
         vm.uid = $sessionStorage.uid; 
+        $scope.newEvent = {};
 
 
-        function createEvent(newEvent) {
-            if (newEvent.name == null || newEvent.description == null){
-                toastr.error('Please enter a name and description!');
+        function createEvent() {
+            if ($scope.newEvent.name === null && newEvent.description === null){
+                toastr.error('Please enter a name and description for your event!');
             } else {
 
-            console.log(newEvent);
-            var postEvent = {
-                creator: vm.uid,  
-                name: newEvent.name, 
-                description: newEvent.description, 
-                requiredUsers : newEvent.requiredUsers, 
-                category : vm.category,
-                timeStamp: Date.now()
-                };
+            console.log($scope.newEvent);
+            // var postEvent = {
+            //     creator: $scope.newEvent.uid,  
+            //     name: $scope.newEvent.name, 
+            //     description: newEvent.description, 
+            //     requiredUsers : newEvent.requiredUsers, 
+            //     category : newEvent.category,
+            //     timeStamp: Date.now()
+            //     };
+
+            $scope.newEvent.creatorId = $sessionStorage.uid;
+            $scope.newEvent.timeStamp = Date.now();
+            console.log($scope.newEvent);
             
 
-            // Get a key for a new event
+            //Get a key for a new event
             var newEventKey = firebase.database().ref().child('events').push().key;
             
             // Write the new post's data simultaneously to the database in 2 places
             var eventUpdates = {};
-            eventUpdates['/events/' + newEventKey] = postEvent;
-            eventUpdates['/users/' + vm.uid + '/user-events/' + newEventKey] = postEvent;
+            eventUpdates['/events/' + newEventKey] = $scope.newEvent;
+            eventUpdates['/users/' + vm.uid + '/user-events/' + newEventKey] = $scope.newEvent;
                 
                 toastr.success('Your event has been posted!');
 
             return firebase.database().ref().update(eventUpdates);
                 }
         }
+
+        $ionicPopover.fromTemplateUrl('js/tabs/create/create.popover.html', {
+             scope: $scope,
+         }).then(function(popover) {
+             $scope.popover = popover;
+         });
+
+         $scope.setCategory = function(category) {
+            
+            
+            alert('test');
+            $scope.newEvent.category = category;
+            console.log($scope.newEvent.category);
+         };
+      
 
        
 
