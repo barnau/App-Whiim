6ee5e53687790534834ac40905433ce023ea55bd
Dreu Stepanik <dreuezekiel@gmail.com>
2016-10-09 18:13:36 -0700

    Edited the current user interface

--- www/js/tabs/notifications/notifications.controller.js
+++ www/js/tabs/notifications/notifications.controller.js
@@ -5,17 +5,32 @@
         .module('app')
         .controller('NotificationsController', NotificationsController);
 
-    NotificationsController.$inject = ['FirebaseFactory', '$scope', '$sessionStorage', 'toastr', '$firebaseObject', '$firebaseArray'];
+    NotificationsController.$inject = ['FirebaseFactory', '$scope', '$sessionStorage', 'toastr', '$firebaseObject', '$firebaseArray', '$ionicPopover'];
+
 
     /* @ngInject */
-    function NotificationsController(FirebaseFactory, $scope, $sessionStorage, toastr, $firebaseObject, $firebaseArray) {
+    function NotificationsController(FirebaseFactory, $scope, $sessionStorage, toastr, $firebaseObject, $firebaseArray, $ionicPopover) {
         var vm = this;
         vm.title = 'NotificationsController';
+        vm.user = {};
         var uid = $sessionStorage.uid;
         console.log('make sure uid is here --> ' + uid);
 
         var ref = firebase.database().ref('/notifications/' + uid);
         $scope.notifications = $firebaseArray(ref);
+        
+        FirebaseFactory.returnUserFromDB(uid).then(function(user) {
+                vm.user = user;
+                console.log(vm.user);
+                
+        })
+
+        $ionicPopover.fromTemplateUrl('js/tabs/notifications/notifications.popover.html', {
+             scope: $scope,
+        }).then(function(popover) {
+             $scope.popover = popover;
+        });
+
         activate();
 
         ////////////////
